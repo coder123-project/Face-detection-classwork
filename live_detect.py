0390e687c8fb01a2a3383312fcbed55c1f21272c
coder123-project <83872425+coder123-project@users.noreply.github.com>
2023-10-17 05:33:56 +0530

    Add files via upload

---
+++ live_detect.py
@@ -0,0 +1,19 @@
+# import the opencv library
+
+# Define a video capture object
+
+
+   
+    # Capture the video frame by frame
+ 
+
+    # Display the resulting frame
+
+      
+    # Quit Window by Spacebar Key
+  
+  
+# After the loop release the cap object
+
+
+# Destroy all the windows
